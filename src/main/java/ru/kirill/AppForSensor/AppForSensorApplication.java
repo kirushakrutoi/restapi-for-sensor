28618a43f41027ad0408084260d8e547a55503c6
kirushakrutoi <kirillpadurin@gmail.com>
2024-02-22 11:09:18 +0300

    create repo

---
+++ src/main/java/ru/kirill/AppForSensor/AppForSensorApplication.java
@@ -0,0 +1,13 @@
+package ru.kirill.AppForSensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppForSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppForSensorApplication.class, args);
+	}
+
+}
